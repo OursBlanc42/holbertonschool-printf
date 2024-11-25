ba7b53539af55f586351eea53a08b28077694f22
OursBlanc42 <simon.regnier@holbertonstudents.com>
2024-11-25 21:04:10 +0100

    Complete code rewrite (using the “mini printf” layout) and addition of a todolist in README.md

--- README.md
+++ README.md
@@ -1,3 +1,17 @@
+## TODO for _printf.c
 
-Afd
- holbertonschool-printf
+  [] update with case percent 
+  [] update with case string
+  [] add special case if malloc fail
+  [] add counter to return number of character display (check man printf)
+  [] valgrind check
+  [] test
+  [] manual review
+  [] comments and formatting / layout
+  [] betty compliance ? (maybe not now ?)
+  [] separate function for each type in another .c file ? */
+
+## TODO for other
+
+  [] prepare man for _printf
+  [] write a nice README.md
--- _printf.c
+++ _printf.c
@@ -1,45 +1,104 @@
-#include <unistd.h>
+#include <stdarg.h> /* to use variadic arguments */
+#include <stdio.h> /* to test with real printf */
+#include <unistd.h> /* to use write function*/
+#include <stdlib.h> /* malloc et free */
 
-int _printf(const char *format)
-{
-  /* declare variable */
-  unsigned int str_len = 0;
-  unsigned int cursor = 0;
-  unsigned int previous_cursor = 0;
-  char *ptr = "A";
-  
-  /* Loop through format until end */
-  while (format[str_len] != '\0')
-  {
-    /* if %c is deteced */
-    if ((format[str_len]=='%') && (format[str_len+1]=='c'))
-    {  /* print from begining to %c */
-      write(1,&format[previous_cursor],cursor);
-      write(1,ptr,1);
-      previous_cursor = cursor +2;
-    }
-    cursor++;
-    str_len++;
-  }
-  
-  /** Write function to display text. We use this syntax :
-  * int write(int fileDescriptor, void *buffer, size_t bytesToWrite)
-  * with file descriptor :
-  * 0    stdin
-  * 1    stdout
-  * 2    stderr
-  */
+/* Define function for each type */
 
-  /* write from %c to end */
-  write(1,&format[previous_cursor],str_len-(previous_cursor));
-
-  return (0);
-
+void print_char(va_list args)
+{ /* char arguments are promoted to int in variadic function */
+  /* so va_arg must use int to retrieve them. */
+	int *ptr = NULL;  /* declare NULL pointer */
+  ptr = malloc(sizeof(char)); /* define size for one char */
+  *ptr = va_arg(args, int); /* write the args in ptr (see top comments to see why is "int")*/
+  write(1,ptr,1); /* write the char */
+  free(ptr); /* free memory */
 }
 
-int main(void)
+void print_string(va_list args) /* TODO : adapt print_char function */
+{   /* code below is wrong, will be corected in futur */
+  printf("%d", va_arg(args, int));
+}
+
+void print_percent(va_list args)    /* TODO : adapt print_char function */
+{   /* code below is wrong, will be corected in futur */
+  printf("%d", va_arg(args, int));
+}
+
+/**
+ * _printf - Emulate printf function
+ * Description : _printf display string with specified format (see exemple) 
+ * @format: list of types of arguments passed to the function
+ * Exemple : _printf("The first letter of alphabet is : %c", 'A');
+ * Output : 'The first letter of alphabet is A'
+ * Return: number of character printed
+ */
+int _printf(const char * format, ...)
 {
-  _printf("Mon premier %c test !\n");
-  _printf("%c %c");
-  return (0);
+  /* Variable declaration and initialisation */
+  int j;
+    unsigned int cursor = 0;
+  unsigned int p_cursor = 0;
+  /* Correspondance table (type specifier and corresponding func) */
+  char types[] = { 'c', 'f', '%', '\0' };
+  /* Array of function pointers, each expecting a va_list argument */
+  void (*print_func[])(va_list) = {
+    print_char,		/* Prints a char */
+    print_string,	/* Prints a string */
+    print_percent  /* Prints a percent */
+    };
+
+  va_list args; /* Arguments list declaration */
+
+  va_start(args, format); /* Initialize list with fixed arguments */
+
+
+
+  /* Loop through format until end */
+  while (format[cursor] != '\0')
+  {
+    /* if cursor found a ''%'' */
+    if (format[cursor]=='%')
+    {
+      /* if % is detected : print until cursor */
+      write(1,&format[p_cursor],(cursor-p_cursor));
+
+     /* check if followed character is on the type list */
+      j = 0; /* initialize J to start the type list at the begining */
+      while (types[j] != '\0')
+      {
+        if (format[cursor+1] == types[j])
+        {  /* call associated function */
+          print_func[j](args);
+          break; /* Exit loop after finding match */
+        }
+        j++; /* increment J for while loop */
+      }
+      /* update cursor to skip ''%X' */
+      cursor = cursor + 2;
+      /* update p_cursor */
+      p_cursor = cursor;
+    }
+    else
+    {
+      cursor++;
+    }
+
+  }
+
+  /* if cursor found last character */
+  if (format[cursor]=='\0')
+  {  /* display the rest */
+    write(1,&format[p_cursor],(cursor-p_cursor));
+  }
+
+  va_end(args); /* Ends the use of the list */
+  return(0);
+}
+
+int main()
+{
+  _printf("b majuscule : %c et d majuscule : %c \n", 'B', 'D');
+  _printf("%c%c",'E','F');
+    return (0);
 }
