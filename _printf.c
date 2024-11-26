1fc6ee714c6e3050cc666a449076240293c3a60b
OursBlanc <regniersimon@hotmail.com>
2024-11-26 15:30:24 +0100

    move print_char from _printf to function_print.c

--- _printf.c
+++ _printf.c
@@ -6,15 +6,6 @@
 
 /* Define function for each type */
 
-void print_char(va_list args)
-{ /* char arguments are promoted to int in variadic function */
-  /* so va_arg must use int to retrieve them. */
-	int *ptr = NULL;  /* declare NULL pointer */
-  ptr = malloc(sizeof(char)); /* define size for one char */
-  *ptr = va_arg(args, int); /* write the args in ptr (see top comments to see why is "int")*/
-  write(1,ptr,1); /* write the char */
-  free(ptr); /* free memory */
-}
 
 void print_string(va_list args) /* TODO : adapt print_char function */
 {   /* code below is wrong, will be corected in futur */
--- function_print.c
+++ function_print.c
@@ -18,3 +18,17 @@ void print_string(va_list args)
 	}
 	write(1, ptr, i);
 }
+
+/**
+ * print_char - print a char
+ *@args: arg receive for print the char
+ */
+void print_char(va_list args)
+{ /* char arguments are promoted to int in variadic function */
+  /* so va_arg must use int to retrieve them. */
+	int *ptr = NULL;  /* declare NULL pointer */
+  ptr = malloc(sizeof(char)); /* define size for one char */
+  *ptr = va_arg(args, int); /* write the args in ptr (see top comments to see why is "int")*/
+  write(1,ptr,1); /* write the char */
+  free(ptr); /* free memory */
+}
