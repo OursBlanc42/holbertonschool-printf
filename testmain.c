833d7453f923406df4abe2e8251837afaef0baf2
OursBlanc <regniersimon@hotmail.com>
2024-11-27 12:07:21 +0100

    update with special case %% et %c

--- testmain.c
+++ testmain.c
@@ -16,8 +16,8 @@ int main(void)
 	printf("printf\t test 2.1 : b et d tout collé : %c%c \n", 'B', 'D');
 	_printf("_printf\t test 2.1 : b et d tout collé : %c%c \n", 'B', 'D');
 
-	printf("printf\t test 2.2 : Caractère avec espace : '%c' \n", 'A');
-	_printf("_printf\t test 2.2 : Caractère avec espace : '%c' \n", 'A');
+	printf("printf\t test 2.2 : Caractère avec espace : '%c' \n", 'Z');
+	_printf("_printf\t test 2.2 : Caractère avec espace : '%c' \n", 'Z');
 
 	printf("printf\t test 2.3 : Caractère ASCII 65 : %c \n", 65);
 	_printf("_printf\t test 2.3 : Caractère ASCII 65 : %c \n", 65);
@@ -37,7 +37,29 @@ int main(void)
 	printf("printf\t test 4 : 3 symbole pourcent %%% \n");
 	_printf("_printf\t test 4 : 3 symbole pourcent %%% \n");
 
+	printf("printf\t test 5 : 4 symboles pourcent %%%% \n");
+	_printf("_printf\t test 5 : 4 symboles pourcent %%%% \n");
 
+	printf("printf\t test 6 : 8 symboles pourcent %%%%%%%% \n");
+	_printf("_printf\t test 6 : 8 symboles pourcent %%%%%%%% \n");
+
+	printf("printf\t test 7 : 15 symboles pourcent %%%%%%%%%%%%%%% \n");
+	_printf("_printf\t test 7 : 15 symboles pourcent %%%%%%%%%%%%%%% \n");
+
+	printf("printf\t test 8 : pourcent début %% milieu %% fin %%\n");
+	printf("printf\t test 8 : pourcent début %% milieu %% fin %%\n");
+
+	printf("printf\t test 9 : mix de pourcent et char : %% et %c \n",'Z');
+	_printf("printf\t test 9 : mix de pourcent et char : %% et %c \n",'Z');
+
+	printf("printf\t test 10 : mix de pourcent et char tout collé à la suite : %%%c \n",'Z');
+	_printf("printf\t test 10 : mix de pourcent et char tout collé à la suite : %%%c \n",'Z');
+
+	printf("printf\t test 11 : mix de pourcent et char tout collé à la suite x2 : %%%c%%%c  \n",'Z','y');
+	_printf("printf\t test 11 : mix de pourcent et char tout collé à la suite x2 : %%%c%%%c \n",'Z','y');
+
+	printf("printf\t test 12 : Pourcent malformé (1 seul symbole) : % \n"); 
+	_printf("_printf\t test 12 : Pourcent malformé  (1 seul symbole) : % \n");
 
 	return (0);
 }
