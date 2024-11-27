2d066052a98acab3449109411da2ccec2c935084
OursBlanc <regniersimon@hotmail.com>
2024-11-27 11:31:03 +0100

    Add test file to test _printf.c

--- testmain.c
+++ testmain.c
@@ -9,9 +9,35 @@
  */
 int main(void)
 {
+	
+	printf("printf\t test 1 : b majuscule : %c et d majuscule : %c \n", 'B', 'D');
+	_printf("_printf\t test 1 : b majuscule : %c et d majuscule : %c \n", 'B', 'D');
+
+	printf("printf\t test 2.1 : b et d tout collé : %c%c \n", 'B', 'D');
+	_printf("_printf\t test 2.1 : b et d tout collé : %c%c \n", 'B', 'D');
+
+	printf("printf\t test 2.2 : Caractère avec espace : '%c' \n", 'A');
+	_printf("_printf\t test 2.2 : Caractère avec espace : '%c' \n", 'A');
+
+	printf("printf\t test 2.3 : Caractère ASCII 65 : %c \n", 65);
+	_printf("_printf\t test 2.3 : Caractère ASCII 65 : %c \n", 65);
+
+	printf("printf\t test 2.4 : Saut de ligne comme caractère : [%c] \n", '\n');
+	_printf("_printf\t test 2.4 : Saut de ligne comme caractère : [%c] \n", '\n');
+
+	printf("printf\t test 2.5 : Tabulation comme caractère : [%c] \n", '\t');
+	_printf("_printf\t test 2.5 : Tabulation comme caractère : [%c] \n", '\t');
+
+	printf("printf\t test 2.6 : Backslash comme caractère : [%c] \n", '\\');
+	_printf("_printf\t test 2.6 : Backslash comme caractère : [%c] \n", '\\');
+
+	printf("printf\t test 3 : pourcent %% \n");
+	_printf("_printf\t test 3 : pourcent %% \n");
+
+	printf("printf\t test 4 : 3 symbole pourcent %%% \n");
+	_printf("_printf\t test 4 : 3 symbole pourcent %%% \n");
 
 
-  printf("printf\t b majuscule : %c et d majuscule : %c \n", 'B', 'D');
-  _printf("_printf\t b majuscule : %c et d majuscule : %c \n", 'B', 'D');
-  return (0);
+
+	return (0);
 }
