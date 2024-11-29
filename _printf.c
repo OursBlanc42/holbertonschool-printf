8b09bf53489333f2ba362357b56e6542b23a231c
Tefa95100 <damien.julien.95100@gmail.com>
2024-11-29 18:19:00 +0100

    Add a check if % is the last char

--- _printf.c
+++ _printf.c
@@ -23,19 +23,23 @@ int comp_char(char letter, va_list args)
 	int j = 0; /*Declare and initialize j*/
 	int count = 0;
 
+	if (letter == '\0')
+	{
+		return (-1);
+	}
 	while (function_search[j].type != '\0')
 	{
 		if (letter == function_search[j].type)
 		{  /* call associated function */
 			/* -2 to consider the %X we don't actually write*/
-			count += function_search[j].func(args) - 2;
+			count += function_search[j].func(args);
 			break; /* Exit loop after finding match */
 		}
 		j++; /* increment J for while loop */
 	}
 	if (function_search[j].type == '\0')
 	{
-		write(1, "%", 2);
+		write(1, "%", 1);
 		write(1, &letter, 1);
 	}
 	return (count);
@@ -55,6 +59,7 @@ int _printf(const char *format, ...)
 	unsigned int cursor = 0;
 	unsigned int p_cursor = 0;
 	int count_function = 0;
+	int verif_error = 0;
 	va_list args; /* Arguments list declaration */
 
 	va_start(args, format); /* Initialize list with fixed arguments */
@@ -67,19 +72,26 @@ int _printf(const char *format, ...)
 			/* if % is detected : print until cursor */
 			write(1, &format[p_cursor], (cursor - p_cursor));
 			/* check if followed character is on the type list */
-			count_function += comp_char(format[cursor + 1], args);
+			verif_error = comp_char(format[cursor + 1], args);
+			if (verif_error < 0)
+			{
+				return (-1);
+			}
+			else
+			{
+				count_function += verif_error - 2;
+			}
 			cursor = cursor + 2;  /* update cursor to skip ''%X' */
 			p_cursor = cursor;  /* update p_cursor */
 		}
-	else
-	{
-		cursor++;
+		else
+		{
+			cursor++;
+		}
 	}
-	}
-	/* if cursor found last character */
-	if (format[cursor] == '\0')
+	if (format[cursor] == '\0')	/* if cursor found last character */
 	{  /* display the rest */
-	write(1, &format[p_cursor], (cursor - p_cursor));
+		write(1, &format[p_cursor], (cursor - p_cursor));
 	}
 	va_end(args); /* Ends the use of the list */
 	return (cursor + count_function); /*Return count of format + count char add*/
